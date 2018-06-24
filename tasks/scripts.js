30d6108a3f89e5ed43bae9f31c094d9896fbe7b2
Steffen Rademacker <kontakt@webgefrickel.de>
2018-06-24 12:18:14 +0200

    Update rollup dependecies, fix scripts/serviceworker build tasks

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -5,7 +5,7 @@ import { rollup } from 'rollup';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import strip from 'rollup-plugin-strip';
 import json from 'rollup-plugin-json';
 import { minify } from 'uglify-es';
