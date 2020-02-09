6e63b964ed2d7b6b9ef13621aaec02b8375a7128
Steffen Rademacker <kontakt@webgefrickel.de>
2020-02-09 22:52:17 +0100

    Fix data example for kalong

--- public/site/plugins/kirby-kalong/src/data.php
+++ public/site/plugins/kirby-kalong/src/data.php
@@ -22,74 +22,19 @@ function kalong($pattern = null, $page = null) {
   $data['pageTitle'] = $page->title() . ' — ' . $home->seotitle();
   $data['pageDescription'] = $page->seodescription();
 
-  // logo, global data
-  $data['logo'] = [];
-  $data['logo']['text'] = implode(' <br />', explode(' ', $site->title()->value()));
-  $data['logo']['label'] = 'zurück zur Startseite';
-
   // navigation objects
   $data['nav'] = [];
   $data['nav']['main'] = [];
-  $data['nav']['meta'] = [];
-  $data['nav']['footer'] = [];
 
   // loop through pages, build nav
-  $data['nav']['main'][] = [
-    'url' => 'projekte/alle-projekte',
-    'label' => 'Alle Projekte',
-  ];
-  foreach ($site->children()->listed()->find('projekte')->tags()->split() as $category) {
+  foreach ($site->children()->listed() as $page) {
     $data['nav']['main'][] = [
-      'url' => 'projekte/' . Str::slug($category),
-      'label' => $category,
+      'url' => $category->url(),
+      'label' => $category->title(),
     ];
   }
 
-  foreach ($site->children()->listed()->not('projekte') as $p) {
-    $data['nav']['meta'][] = [
-      'url' => $p->url(),
-      'label' => $p->title(),
-    ];
-  }
-
-  foreach ($site->children()->unlisted()->filterBy('template', 'in', ['meta']) as $p) {
-    $data['nav']['footer'][] = [
-      'url' => $p->url(),
-      'label' => $p->title(),
-    ];
-  }
-
-  // INTRO is global from $home
-  $data['intro'] = [];
-  $data['intro']['introgallery'] = [];
-  $data['intro']['introgallery']['modifiers'] = 'intro__gallery';
-  $data['intro']['introgallery']['gallery'] = [];
-
-  $data['intro']['news'] = false;
-  if ($home->isnews()->bool()) {
-    $data['intro']['news'] = [
-      'date' => $home->date()->toDate('%d.%m.%Y'),
-      'datetime' => $home->date()->toDate('%d-%m-%YT10:00+02:00'),
-      'content' => $home->text()->kirbytext(),
-    ];
-  }
-
-  $intro = $home->intro()->toStructure();
-  foreach ($intro as $i) {
-    $link = $i->link()->toLink();
-    $isLinked = ($link !== null);
-    $image = $home->files()->first()->kalongImage('gallery__image', '(min-width: 960px) 67vw, 100vw');
-    $image['href'] = ($isLinked) ? $link : false;
-    $image['type'] = 'image';
-    $data['intro']['introgallery']['gallery'][] = $image;
-  }
-
-  // BACK, global default
-  $data['back'] = [
-    'url' => 'javascript:history.back()',
-    'label' => 'Zurück',
-  ];
-
+  // AND MORE GLOBAL DATA—Change this file, if you want to!
 
   return $data;
 }
