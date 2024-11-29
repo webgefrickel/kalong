23f25513e89586c50823c8a5870a5d2bafaa7bda
Steffen Rademacker <kontakt@webgefrickel.de>
2024-11-29 11:14:29 +0100

    Yeah

--- TODO.md
+++
@@ -1,4 +0,0 @@
-# Known issues
-
-- Documentation & Examples
-- Promise/async/await-chain in build.js: "Done" will be logged before postBuild task is finished.
--- src/scripts/3-global/lazyload.js
+++
@@ -1,7 +0,0 @@
-import lozad from 'lozad';
-
-export default () => {
-  // this loads videos as well
-  const observer = lozad(document.querySelectorAll('[data-src], [data-srcset], [data-poster]'));
-  observer.observe();
-};
